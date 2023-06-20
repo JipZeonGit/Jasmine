5883050e812753799b4bf0a1415cdc3cfa7cd8e2
JipZeonGit <1416176707@qq.com>
2023-06-20 19:24:16 +0800

    新增 角色管理 模块

--- src/main/java/com/nfu/jasmine/sys/controller/RoleController.java
+++ src/main/java/com/nfu/jasmine/sys/controller/RoleController.java
@@ -1,18 +1,83 @@
 package com.nfu.jasmine.sys.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.nfu.jasmine.common.vo.Result;
+import com.nfu.jasmine.sys.entity.Role;
+import com.nfu.jasmine.sys.service.IRoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author jipzeongit
  * @since 2023-05-29
  */
-@Controller
-@RequestMapping("/sys/role")
+@Api(tags = {"角色接口列表"})
+@RestController
+@RequestMapping("/role")
 public class RoleController {
+
+    @Autowired
+    private IRoleService roleService;
+
+    @ApiOperation("获取全部角色")
+    @GetMapping("/list")
+    public Result<Map<String,Object>> getUserList(@RequestParam(value = "roleName",required = false) String roleName,
+                                                  @RequestParam(value = "pageNo") Long pageNo,
+                                                  @RequestParam(value = "pageSize") Long pageSize){
+        LambdaQueryWrapper<Role> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(StringUtils.hasLength(roleName),Role::getRoleName,roleName);
+        wrapper.orderByDesc(Role::getRoleId);
+
+        Page<Role> page = new Page<>(pageNo,pageSize);
+        roleService.page(page, wrapper);
+
+        Map<String,Object> data = new HashMap<>();
+        data.put("total",page.getTotal());
+        data.put("rows",page.getRecords());
+
+        return Result.success(data);
+
+    }
+
+    @ApiOperation("新增角色")
+    @PostMapping
+    public Result<?> addRole(@RequestBody Role role){
+        roleService.save(role);
+        return Result.success("新增角色成功");
+    }
+
+    @ApiOperation("修改角色")
+    @PutMapping
+    public Result<?> updateRole(@RequestBody Role role){
+        roleService.updateById(role);
+        return Result.success("修改角色成功");
+    }
+
+    @ApiOperation("根据ID查询单个角色")
+    @GetMapping("/{id}")
+    public Result<Role> getRoleById(@PathVariable("id") Integer id){
+        Role role = roleService.getById(id);
+        return Result.success(role);
+    }
+
+    @ApiOperation("根据ID逻辑删除角色数据")
+    @DeleteMapping("/{id}")
+    public Result<Role> deleteRoleById(@PathVariable("id") Integer id){
+        roleService.removeById(id);
+        return Result.success("删除角色数据成功");
+    }
 
 }
