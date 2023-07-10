b10eba39884b88f888f035a90721d01ca9e608b7
JipZeonGit <1416176707@qq.com>
2023-07-10 21:45:42 +0800

    更新 门店客户 模块

--- src/main/java/com/nfu/jasmine/cus/controller/AppointmentController.java
+++ src/main/java/com/nfu/jasmine/cus/controller/AppointmentController.java
@@ -1,7 +1,15 @@
 package com.nfu.jasmine.cus.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.nfu.jasmine.common.vo.Result;
+import com.nfu.jasmine.cus.entity.Appointment;
+import com.nfu.jasmine.cus.service.IAppointmentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +19,45 @@ import org.springframework.stereotype.Controller;
  * @author jipzeongit
  * @since 2023-07-06
  */
-@Controller
-@RequestMapping("/cus/appointment")
+@Api(tags = {"预约接口列表"})
+@RestController
+@RequestMapping("/appointment")
 public class AppointmentController {
+    @Autowired
+    private IAppointmentService appointmentService;
 
+    @ApiOperation("获取全部预约")
+    @GetMapping("/all")
+    public Result<List<Appointment>> getAllAppointment(){
+        List<Appointment> list = appointmentService.list();
+        return Result.success(list,"查询成功");
+    }
+
+    @ApiOperation("新增预约")
+    @PostMapping("")
+    public Result<?> addAppointment(@RequestBody Appointment appointment){
+        appointmentService.save(appointment);
+        return Result.success("新增预约成功！");
+    }
+
+    @ApiOperation("修改预约")
+    @PutMapping("")
+    public Result<?> updateAppointment(@RequestBody Appointment appointment){
+        appointmentService.updateById(appointment);
+        return Result.success("修改预约成功！");
+    }
+
+    @ApiOperation("根据ID查询预约")
+    @GetMapping("/{id}")
+    public Result<Appointment> getAppointmentById(@PathVariable("id") Integer id){
+        Appointment appointment = appointmentService.getById(id);
+        return Result.success(appointment);
+    }
+
+    @ApiOperation("根据ID逻辑删除预约数据")
+    @DeleteMapping("/{id}")
+    public Result<Appointment> deleteAppointmentById(@PathVariable("id") Integer id){
+        appointmentService.removeById(id);
+        return Result.success("删除预约数据成功！");
+    }
 }
--- src/main/java/com/nfu/jasmine/cus/controller/VipController.java
+++ src/main/java/com/nfu/jasmine/cus/controller/VipController.java
@@ -1,7 +1,16 @@
 package com.nfu.jasmine.cus.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.nfu.jasmine.common.vo.Result;
+import com.nfu.jasmine.cus.entity.Appointment;
+import com.nfu.jasmine.cus.entity.Vip;
+import com.nfu.jasmine.cus.service.IVipService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +20,45 @@ import org.springframework.stereotype.Controller;
  * @author jipzeongit
  * @since 2023-07-06
  */
-@Controller
-@RequestMapping("/cus/vip")
+@Api(tags = {"会员接口列表"})
+@RestController
+@RequestMapping("/vip")
 public class VipController {
+    @Autowired
+    private IVipService vipService;
 
+    @ApiOperation("获取全部会员")
+    @GetMapping("/all")
+    public Result<List<Vip>> getAllVip(){
+        List<Vip> list = vipService.list();
+        return Result.success(list,"查询成功");
+    }
+
+    @ApiOperation("新增会员")
+    @PostMapping("")
+    public Result<?> addVip(@RequestBody Vip vip){
+        vipService.save(vip);
+        return Result.success("新增预约成功！");
+    }
+
+    @ApiOperation("修改会员")
+    @PutMapping("")
+    public Result<?> updateVip(@RequestBody Vip vip){
+        vipService.updateById(vip);
+        return Result.success("修改会员成功！");
+    }
+
+    @ApiOperation("根据ID查询会员")
+    @GetMapping("/{id}")
+    public Result<Vip> getVipById(@PathVariable("id") Integer id){
+        Vip vip = vipService.getById(id);
+        return Result.success(vip);
+    }
+
+    @ApiOperation("根据ID逻辑删除会员数据")
+    @DeleteMapping("/{id}")
+    public Result<Appointment> deleteVipById(@PathVariable("id") Integer id){
+        vipService.removeById(id);
+        return Result.success("删除会员数据成功！");
+    }
 }
