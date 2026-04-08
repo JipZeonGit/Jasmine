4e5185933352071d2726e02d60f51fa560aa5aa8
JipZeonGit <jipzeongit@qq.com>
2026-04-08 22:15:37 +0800

    test: 避免 IT 之间复用旧测试上下文

--- src/test/java/com/nfu/jasmine/config/AbstractIntegrationTest.java
+++ src/test/java/com/nfu/jasmine/config/AbstractIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.nfu.jasmine.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -13,6 +14,7 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest
 @ActiveProfiles("integration")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class AbstractIntegrationTest {
 
     @Container
